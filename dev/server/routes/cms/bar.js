b25d04109150ca8b318ade7e7a39229b2e2ffe04
Brandon Da Silva <brandon@bdsdesign.co>
2018-01-05 23:55:17 -0500

    Edits

--- dev/server/routes/cms/bar.js
+++ dev/server/routes/cms/bar.js
@@ -10,11 +10,11 @@ var webflow = new Webflow({ token: process.env.WEBFLOW_TOKEN });
 
 router.get ('/', function(req, res) {
   res.set('Access-Control-Allow-Origin', '*');
-  
+
   var bar = webflow.items({ collectionId: process.env.WEBFLOW_BAR_ID });
 
   bar.then(function(i){
-    res.send({ "bar-count": i.count; });
+    res.send({ "bar-count": i.count });
   });
 });
 
